391fd070a1ababbdff4206940ab079c335f5ad41
Alex Szefezuk <92179905+AlexSzefezuk@users.noreply.github.com>
2025-03-17 11:24:53 +0000

    desafio mestre

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -60,6 +60,57 @@ void posicionaNavioDiagonalEsquerda(int tabuleiro[LINHAS][COLUNAS], int posicaoC
 
 };
 
+void cone(int tabuleiro[LINHAS][COLUNAS], int y, int x) {
+    int altura = 3;
+    int largura = 5;
+
+    for (int i = 0; i < altura; i++) {
+        int inicio = y - i;
+        int fim = y + i;
+
+        for (int j = inicio; j <= fim; j++) {
+            if (j >= 0 && j < 10 && (x + i) < 10) {
+                tabuleiro[x + i][j] = 3;
+            }
+        }
+    }
+}
+
+void cruz(int tabuleiro[LINHAS][COLUNAS]) {
+    int meio = LINHAS / 2; 
+    int largura = COLUNAS / 2;
+
+    for (int i = 0; i < LINHAS; i++) {
+        for(int j = 0; j < COLUNAS; j++) {
+            if (i == meio || j == largura) {
+                tabuleiro[i][j] = 3;
+            }
+
+        }
+    }
+}
+
+void octaedro(int tabuleiro[LINHAS][COLUNAS], int y, int x) {
+    int altura = 3;
+    int largura = 3;
+
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            if (
+            (i == y && j == x) || 
+            (i == y - 1 && j == x) || 
+            (i == y + 1 && j == x) || 
+            (i == y && j == x - 1) || 
+            (i == y && j == x + 1)) {
+            tabuleiro[i][j] = 3;
+        }
+
+        }
+    }
+}
+
+
+
 int main() {
     int tabuleiro[LINHAS][COLUNAS];
 
@@ -76,6 +127,10 @@ int main() {
     posicionaNavioVertical(tabuleiro, 3, 5);
     posicionaNavioDiagonalDireita(tabuleiro, 7, 3);
     posicionaNavioDiagonalEsquerda(tabuleiro, 5, 7);
+
+    cone(tabuleiro, 5, 5);
+    cruz(tabuleiro);
+    octaedro(tabuleiro, 2, 2);
 
     imprimeTabuleiro(tabuleiro);
 
